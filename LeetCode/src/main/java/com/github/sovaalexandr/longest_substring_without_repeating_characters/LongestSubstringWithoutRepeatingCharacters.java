40c7e15063db47a968835d1390f205e5e64b66d3
sovaalexandr <sovaalexandr@gmail.com>
2019-01-22 02:27:04 +0200

    longest-substring-without-repeating-characters: Explanation

--- LeetCode/src/main/java/com/github/sovaalexandr/longest_substring_without_repeating_characters/LongestSubstringWithoutRepeatingCharacters.java
+++ LeetCode/src/main/java/com/github/sovaalexandr/longest_substring_without_repeating_characters/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,7 +1,6 @@
 package com.github.sovaalexandr.longest_substring_without_repeating_characters;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * {@see https://leetcode.com/problems/longest-substring-without-repeating-characters/}
@@ -18,8 +17,8 @@ public class LongestSubstringWithoutRepeatingCharacters
         for(i = 0, j = 0; j < chars.length; j++) {
             prevIdx = seen.put(chars[j], j);
             if (null != prevIdx && i <= prevIdx) { // This char is already seen
-                max = Math.max(max, j - i);
-                i = prevIdx + 1;
+                max = Math.max(max, j - i); // Check isn't it a longest subsequence
+                i = prevIdx + 1; // Move left border
             }
         }
 
