4c6014026c252ff3d5980db89c040b20b6866e85
sovaalexandr <sovaalexandr@gmail.com>
2019-09-26 02:40:07 +0300

    Middle of the Linked List

---
+++ LeetCode/src/main/java/com/github/sovaalexandr/middle_of_the_linked_list/MiddleOfTheLinkedList.java
@@ -0,0 +1,17 @@
+package com.github.sovaalexandr.middle_of_the_linked_list;
+
+import com.github.sovaalexandr.ListNode;
+
+public class MiddleOfTheLinkedList
+{
+    public ListNode middleNode(ListNode head) {
+        if (null == head || null == head.next) return head;
+
+        ListNode fast = head;
+        while (null != fast && null != fast.next) {
+            fast = fast.next.next;
+            head = head.next;
+        }
+        return head;
+    }
+}
